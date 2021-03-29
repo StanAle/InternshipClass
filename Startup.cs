694966bd17f53ed2d8f51a258c54bd389427885d
AzureAD\Alexandra-GeorgianaS <alexandra.stan@principal33.com>
2021-03-29 12:37:57 +0300

    Added swagger in the main project

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace InternshippClass
 {
@@ -26,6 +27,10 @@ namespace InternshippClass
         {
             services.AddControllersWithViews();
             services.AddSingleton(typeof(InternshipService));
+			services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "InternshipClass.WebAPI", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +39,9 @@ namespace InternshippClass
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InternshipClass.WebAPI v1"));
+
             }
             else
             {
