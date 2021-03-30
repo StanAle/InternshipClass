585f251dfa4cf5e361666b5defb9896acaea733b
AzureAD\Alexandra-GeorgianaS <alexandra.stan@principal33.com>
2021-03-31 00:18:11 +0300

    Converted json response to WeatherForecast list

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using InternshippClass.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 
 namespace InternshipMvc.WebApi.Controllers
 {
@@ -17,6 +21,7 @@ namespace InternshipMvc.WebApi.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
@@ -32,13 +37,47 @@ namespace InternshipMvc.WebApi.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
+
+            var client = new RestClient("https://api.openweathermap.org/data/2.5/onecall?lat=45.75&lon=25.3333&exclude=hourly,daily&appid=432ab032de75c6c0656eb54a71e44a1d");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
+                //TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)],
             })
             .ToArray();
+        }
+
+        public IList<WeatherForecast> FetchWeatherForecasts(double latitude, double longitude, string apiKey)
+        {
+            var client = new RestClient($"https://api.openweathermap.org/data/2.5/onecall?lat={latitude}&lon={longitude}&exclude=hourly,minutely&appid={apiKey}");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            return ConvertResponseContentToWeatherForecasts(response.Content);
+        }
+
+        private IList<WeatherForecast> ConvertResponseContentToWeatherForecasts(string content)
+        {
+            JToken root = JObject.Parse(content);
+            JToken testToken = root["daily"];
+            IList<WeatherForecast> forecasts = new List<WeatherForecast>();
+            foreach (var token in testToken)
+            {
+                var forecast = new WeatherForecast();
+                forecast.Date = DateTimeConverter.ConvertEpochToDayTime(long.Parse(token["dt"].ToString()));
+                forecast.TemperatureK = double.Parse(token["temp"]["day"].ToString());
+                forecast.Summary = token["weather"][0]["description"].ToString();
+                forecasts.Add(forecast);
+            }
+
+            return forecasts;
         }
     }
 }
--- InternshipClass.Tests/DayTest.cs
+++ InternshipClass.Tests/DayTest.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using InternshipMvc.WebApi;
+using InternshippClass.Utilities;
+using InternshipMvc.WebApi.Controllers;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace InternshipClass.Tests
 {
@@ -16,19 +20,31 @@ namespace InternshipClass.Tests
             long ticks = 1617184800;
 
             // Act
-            DateTime dateTime = ConvertEpochToDayTime(ticks);
+            DateTime dateTime = DateTimeConverter.ConvertEpochToDayTime(ticks);
 
             // Assert
             Assert.Equal(2021, dateTime.Year);
             Assert.Equal(3, dateTime.Month);
             Assert.Equal(31, dateTime.Day);
         }
-
-        private static DateTime ConvertEpochToDayTime(long ticks)
+        
+        [Fact]
+        public void ConvertOutputOfWeatherAPIToWeatherForecast()
         {
-            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(ticks);
-            DateTime dateTime = dateTimeOffset.UtcDateTime;
-            return dateTime;
+            // Assume
+            //https://api.openweathermap.org/data/2.5/onecall?lat=45.75&lon=25.3333&exclude=hourly,minutely&appid=432ab032de75c6c0656eb54a71e44a1d
+            double latitude = 45.75;
+            double longitude = 25.3333;
+            var apiKey = "432ab032de75c6c0656eb54a71e44a1d";
+            Microsoft.Extensions.Logging.ILogger<WeatherForecastController> nullLogger = new NullLogger<WeatherForecastController>();
+            var weatherForecastController = new WeatherForecastController(nullLogger);
+
+            // Act
+            var weatherForecasts = weatherForecastController.FetchWeatherForecasts(latitude,longitude,apiKey);
+            WeatherForecast weatherForecast = weatherForecasts[1];
+
+            // Assert
+            Assert.Equal(286.82, weatherForecast.TemperatureK);
         }
     }
 }
