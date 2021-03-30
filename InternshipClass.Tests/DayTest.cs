fc4add55d467cc1d6be86eaf6ee38c5e3e58b846
AzureAD\Alexandra-GeorgianaS <alexandra.stan@principal33.com>
2021-03-30 12:19:31 +0300

    Added test class to check epoch to DateTime conversion

---
+++ InternshipClass.Tests/DayTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace InternshipClass.Tests
+{
+    public class DayTest
+    {
+        [Fact]
+        public void CheckEpochConversion()
+        {
+            // Assume
+            long ticks = 1617184800;
+
+            // Act
+            DateTime dateTime = ConvertEpochToDayTime(ticks);
+
+            // Assert
+            Assert.Equal(2021, dateTime.Year);
+            Assert.Equal(3, dateTime.Month);
+            Assert.Equal(31, dateTime.Day);
+        }
+
+        private static DateTime ConvertEpochToDayTime(long ticks)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(ticks);
+            DateTime dateTime = dateTimeOffset.UtcDateTime;
+            return dateTime;
+        }
+    }
+}
