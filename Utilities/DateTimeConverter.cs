979043dc924f7693cbe09299147b3563cf25646a
AzureAD\Alexandra-GeorgianaS <alexandra.stan@principal33.com>
2021-03-31 00:14:18 +0300

    Added Utilities class to convert epoch to DateTime.

---
+++ Utilities/DateTimeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshippClass.Utilities
+{
+    public static class DateTimeConverter
+    {
+        public static DateTime ConvertEpochToDayTime(long ticks)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(ticks);
+            DateTime dateTime = dateTimeOffset.UtcDateTime;
+            return dateTime;
+        }
+    }
+}
