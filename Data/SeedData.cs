926aa65ca7de37bfe7a6e65aa655d451494b9a41
AzureAD\Alexandra-GeorgianaS <alexandra.stan@principal33.com>
2021-04-20 11:21:42 +0300

    Fixed SeedData to add locations if there are none

--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -14,15 +14,15 @@ namespace InternshippClass.Data
             context.Database.Migrate();
             if (!context.Locations.Any())
             {
-                return;   // DB has been seeded
+                var locations = new Location[]
+                {
+                    defaultLocation = new Location { Name = "Kyiv", NativeName = "Київ", Longitude = 30.5167, Latitude = 50.4333, },
+                    new Location { Name = "Brasov", NativeName = "Braşov", Longitude = 25.3333, Latitude = 45.75, },
+                };
+                context.Locations.AddRange(locations);
+                context.SaveChanges();
             }
 
-            var locations = new Location[]
-            {
-                defaultLocation = new Location { Name = "Kyiv", NativeName = "Київ", Longitude = 30.5167, Latitude = 50.4333, },
-                new Location { Name = "Brasov", NativeName = "Braşov", Longitude = 25.3333, Latitude = 45.75, },
-            };
-            context.Locations.AddRange(locations);
             if (!context.Interns.Any())
             {
                 var interns = new Intern[]
