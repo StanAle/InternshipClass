cbd4a0d45cb0af2a48b6d76bfdaa032838e4c123
AzureAD\Alexandra-GeorgianaS <alexandra.stan@principal33.com>
2021-04-20 12:34:10 +0300

    Fixed some warnings.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -46,10 +46,10 @@ namespace InternshippClass.WebApi.Controllers
         }
 
         /// <summary>
-        /// Getting weather forecast for today + 7 days ahead for a specific location
+        /// Getting weather forecast for today + 7 days ahead for a specific location.
         /// </summary>
-        /// <param name="latitude">In range -90 and 90. For example: latitude for Brasov is 45.75</param>
-        /// <param name="logiude">In range -180 and 180. For example: longitude for Brasov is 25.3333</param>
+        /// <param name="latitude">In range -90 and 90. For example: latitude for Brasov is 45.75.</param>
+        /// <param name="logiude">In range -180 and 180. For example: longitude for Brasov is 25.3333.</param>
         /// <returns>List of weatherForecast objects.</returns>
         [HttpGet("/Forecast")]
         public List<WeatherForecast> Get(double latitude, double logiude)
